9a5a8d31e4ee56dd5fbaee0d2fb9dbb4c3df5f38
thomamas <tinsel@tinsel.org>
2016-11-23 12:57:16 -0800

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,6 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 Chef::Log.warn('The gnops_bind9 cookbook has only been tested with Debian.') \
   unless platform_family?('debian')
